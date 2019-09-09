b7db4223c55415b340965b5945cbe01abe622dea
aafeitosa <alice.cardoso10@gmail.com>
2019-09-09 16:47:11 -0500

    Carregando dados do produto

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProdutoDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { API_CONFIG } from '../../config/api.config';
+import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/produto.service';
 
 @IonicPage()
 @Component({
@@ -15,11 +11,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProdutoDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  item: ProdutoDTO;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProdutoDetailPage');
+    let produto_id = this.navParams.get('produto_id');
+    this.produtoService.findById(produto_id)
+      .subscribe(response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+      },
+      error => {});
   }
 
+  getImageUrlIfExists() {
+    this.produtoService.getImageFromBucket(this.item.id)
+      .subscribe(response => {
+        this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+      },
+      error => {});
+  }
 }
